3ca1da5e0394c257291bb23a532b6d94d6a55714
Brian Roy <brian@briantroy.com>
2012-04-08 10:20:48 -0700

    New is running monitor script.

--- scripts/is_running.sh
+++ scripts/is_running.sh
@@ -1,17 +1,30 @@
-# This script can be used in cron to monitor and automatically restart the
-# AlertServer. This is usually not needed... but for the paranoid (like me)
-# your AlertServer will restart with no human intervention.
-
 #!/bin/sh
-SERVICE='AlertServer'
+SERVICE='AlertServer.jar'
+MAILTO='you@yourdomain.com'
 
-# The directory where you've installed AlertServer.jar
-BASEPATH=/usr/local/AlertServer_v1.2
+NUMFOUND=`ps ax |grep -v grep | grep $SERVICE | wc -l`
 
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
+echo "NOTICE: $(date -R) - Found $NUMFOUND running $SERVICE" >> /usr/local/AlertServer_v1.2/monitor_log
+
+if [ "$NUMFOUND" = "1" ]
 then
-    echo "`date +"%F %r"` - $SERVICE service running, everything is fine." >> $BASEPATH/monitor_log
-else
-    echo "`date +"%F %r"` - $SERVICE is not running. Restarting now..." >> $BASEPATH/monitor_log
-    $BASEPATH/alertServer.sh
+    echo "NOTICE: $(date -R) - $SERVICE service running, everything is fine" >> /usr/local/AlertServer_v1.2/monitor_log
+    exit
+fi
+
+if [ "$NUMFOUND" -gt "1" ]
+then
+    echo "NOTICE: $(date -R) - More than one $SERVICE is running... killing all and restarting." >> /usr/local/AlertServer_v1.2/monitor_log
+    # echo "$(date -R) - $SERVICE is running multiple times... restarting!" | mail -s "$SERVICE down" $MAILTO
+    /usr/local/AlertServer_v1.2/stopAlertServer.sh >> /dev/null
+    killall java
+    /usr/local/AlertServer_v1.2/alertServer.sh >> /dev/null
+    exit
+fi
+
+if [ "$NUMFOUND" = "0" ]
+then
+    echo "ERROR: $(date -R) - $SERVICE is not running and will be restarted..." >> /usr/local/AlertServer_v1.2/monitor_log
+    # echo "$(date -R) - $SERVICE is not running! It will now be restarted..." | mail -s "$SERVICE down" $MAILTO
+    /usr/local/AlertServer_v1.2/alertServer.sh >> /dev/null
 fi
