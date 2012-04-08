ae6126f2b43abc4a6c15c72b63036e0ba12c0d29
Brian Roy <brian@briantroy.com>
2012-04-08 10:12:56 -0700

    Fixing the start script.

--- scripts/alertServer.sh
+++ scripts/alertServer.sh
@@ -18,5 +18,5 @@ sleep 5
 cd $BASEPATH
 
 # Replace the below with the full path to java on your system.
-/opt/jre1.6.0_24/bin/java -jar $BASEPATH/AlertServer.jar -imSvr talk.google.com -imDomain justsignal.com -imSSH yes -listenPort 3030 -imUser alert -imPasswd Is@b3l10 -imResource prod_pbx -configFile $BASEPATH/xmppworker.conf >> /dev/null &
+/opt/jre1.6.0_24/bin/java -jar $BASEPATH/AlertServer.jar -imSvr $XMPPSERVER -imDomain $XMPPDOMAIN -imSSH $USESSH -listenPort 3030 -imUser $XMPPUSER -imPasswd $XMPPPWD -imResource $XMPPRESOURCE -configFile $BASEPATH/xmppworker.conf >> /dev/null &
 echo $! > $BASEPATH/alertServer.pid
